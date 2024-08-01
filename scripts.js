c01ba673eee9593db4721d60a859c2ac24924886
Shashank-Koppella <100149433+Shashank-Koppella@users.noreply.github.com>
2024-08-02 00:33:13 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function createGroupedButton(group, buttonNames) {
         const existingGroupedButton = document.querySelector(`.grouped-button[data-group="${group}"]`);
         const groupNames = {
-            '1': 'Endings of lab softwares',
+            '1': 'Facilities found in Marena',
             '2': 'Things in hostel',
             '3': 'Bluedove Enterprises in HB3',
             '4': 'Words starting with branches in MAHE '
